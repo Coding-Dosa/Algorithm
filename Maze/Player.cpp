5f78a05cea497b8bdc1181f4263f48c63f21c0bc
Coding-Dosa <yejoon146@gmail.com>
2023-06-28 15:08:45 +0900

    Algorithm : right handed rule

--- Maze/Player.cpp
+++ Maze/Player.cpp
@@ -6,8 +6,65 @@ void Player::Init(Board* board)
 {
 	_pos = board->GetEnterPos();
 	_board = board;
+	
+	Pos pos = _pos;
+
+	_path.clear();
+	_path.push_back(pos);
+	
+	Pos dest = board->GetExitPos();
+
+	Pos movementForDirection[4] = 
+	{
+		Pos {-1, 0},
+		Pos {0, -1},
+		Pos {1, 0},
+		Pos {0, 1},
+	};
+	
+	while (pos != dest)
+	{
+		// 현재 바라보는 방향을 기준으로 오른쪽으로 갈 수 있는지 확인
+		int32 newDir = (_dir - 1 + DIR_COUNT) % DIR_COUNT;
+		if (CanGo(pos + movementForDirection[newDir]))
+		{
+			// 오른쪽 방향으로 90도 회전
+			_dir = newDir;
+			// 앞으로 한보 전진
+			pos += movementForDirection[_dir];
+			_path.push_back(pos);
+		}
+		// 현재 바라보는 방향을 기준으로 앞쪽으로 전진할수 있는지 확인
+		else if (CanGo(pos + movementForDirection[_dir]))
+		{
+			// 앞으로 한보 전진
+			pos += movementForDirection[_dir];
+			_path.push_back(pos);
+		}
+		else
+		{
+			// 왼쪽 방향으로 90도 회전
+			_dir = (_dir + 1) % DIR_COUNT;
+		}
+	}
 }
 
 void Player::Update(uint64 deltaTick)
 {
+	if (_pathIndex >= _path.size())
+		return;
+	
+	_sumTick += deltaTick;
+	if (_sumTick > MOVE_TICK)
+	{
+		_sumTick = 0;
+		_pos = _path[_pathIndex];
+		_pathIndex++;
+	}
+}
+
+bool Player::CanGo(Pos pos)
+{
+	TileType tileType = _board->GetTileType(pos);
+	return tileType == TileType::EMPTY;
 }
